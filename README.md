f1ebb345bbf4cbf24a9d19cdf4763f3e4d8b90c3
amywest1 <87080188+amywest1@users.noreply.github.com>
2022-03-24 14:18:54 -0400

    readme and began adding el to weather

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # m6-hw9-west-amy
 Weather Widget
+
+[Ways to represent the Degree Symbol](http://mc-computing.com/Languages/Javascript/Degree_Symbol/Degree_Symbol.html#Ways_to_represent_the_Degree_Symbol).
+
+[How do I use the icon given in the Open Weather API?](https://www.reddit.com/r/FreeCodeCamp/comments/4con5s/how_do_i_use_the_icon_given_in_the_open_weather/)
+
+[Weather icons](https://openweathermap.org/weather-conditions)
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var currentWeather = document.getElementById('weather')
+var weatherEl = document.getElementById('weather')
 var form = document.querySelector('form')
 var cityInput = document.querySelector('input[type=text]')
 
@@ -12,10 +12,31 @@ form.onsubmit = function(e) {
     })
     .then(function(data) {
     console.log(data)
+    
+    var div = document.createElement('div')
+    var h2 = document.createElement('h2')
+    h2.textContent = data.name
+    div.appendChild(h2)  
+    
+    var iconCode = data.weather[0].icon
+    var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+    var img = document.createElement('img')
+    img.src = iconUrl
+    div.appendChild(img)
+
+    var conditions = document.createElement('h2')
+    conditions.textContent = data.weather[0].description
+    div.appendChild(conditions)
+
+    var currentTemp = document.createElement('h2')
+    currentTemp.textContent = "Current: " + data.main.temp + "\xB0 F"
+    div.appendChild(currentTemp)
+    
+    weatherEl.appendChild(div)
     })
-    .catch(function(err) {
-    console.log(err)
-    })
+    // .catch(function(err) {
+    // console.log(err)
+    // })
 }
 
 
