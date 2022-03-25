44d4e9679f79e7220ed7769cbc8653906a01efa1
amywest1 <87080188+amywest1@users.noreply.github.com>
2022-03-25 12:24:46 -0400

    err message

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var weatherEl = document.getElementById('weather')
 var form = document.querySelector('form')
 var cityInput = document.querySelector('input[type=text]')
 
@@ -8,7 +7,7 @@ form.onsubmit = function(e) {
     console.log(cityInput.value)
     fetch('https://api.openweathermap.org/data/2.5/weather?appid=b0b254017f8d7a11c203615608c092a8&units=imperial&q=' + cityInput.value)
     .then(function(response) {
-    return response.json()
+        return response.json()
     })
     .then(function(data) {
     console.log(data)
@@ -50,12 +49,12 @@ form.onsubmit = function(e) {
     updated.textContent = "Last updated: " + date
     div.appendChild(updated)
     
-
-
     weatherEl.appendChild(div)
-
-     
-    })
+})
+.catch(function(error) {
+    console.log(error)
+    document.getElementById('weather').innerHTML = "Location not found."
+})
 
 }
 
